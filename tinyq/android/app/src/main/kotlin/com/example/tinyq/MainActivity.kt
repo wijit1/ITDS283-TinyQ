dada560136b81c0a4b039f485a74f62f94144f01
Sorrakit_Bank <htoo24653@gmail.com>
2025-03-25 21:04:41 +0700

    Setting Project & Make Get Start Page

--- tinyq/android/app/src/main/kotlin/com/example/tinyq/MainActivity.kt
+++ tinyq/android/app/src/main/kotlin/com/example/tinyq/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.tinyq
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
--- tinyq/lib/main.dart
+++ tinyq/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,78 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Color(0xFF225AEB),
+          body: Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 60),
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "TinyQ?",
+                      style: GoogleFonts.pacifico(
+                          fontSize: 80,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding:  EdgeInsets.symmetric(vertical: 3),
+                    child: Image(
+                      image: AssetImage('assets/images/app_icon.png'),
+                      height: 360,
+                      width: 360,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 275,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60)
+                        )
+                      ) ,
+                      onPressed: (){
+                        
+                      }, 
+                      child: Container(
+                        child: Text("Get Start",
+                        style: TextStyle(
+                          fontSize: 25
+                        ),),
+                      )),
+                  ),
+                  SizedBox(height: 30,),
+                  SizedBox(
+                    width: 275,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amberAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60)
+                        )
+                      ) ,
+                      onPressed: (){
+                    
+                      }, 
+                      child: Container(
+                        child: Text("Log in",
+                        style: TextStyle(
+                          fontSize: 25
+                        ),),
+                      )),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
