e281acc69dc14e18a3be4dca343de791ba8382af
Sorrakit_Bank <htoo24653@gmail.com>
2025-03-28 14:51:23 +0700

    Complete UI Login Screen
    Add a button login , Textfield input

--- tinyq/lib/screen/login_screen.dart
+++ tinyq/lib/screen/login_screen.dart
@@ -8,6 +8,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  FocusNode email_F = FocusNode();
+  FocusNode password_F = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,13 +34,69 @@ class _LoginScreenState extends State<LoginScreen> {
                   fontWeight: FontWeight.bold),
             ),
           ),
-          Textfield(),
+          Textfield(email, AssetImage("assets/images/mail_icon.png"), "Email", email_F),
+          Textfield(password, AssetImage("assets/images/locked.png"), "Password", password_F),
+          Login_Button(),
+          Have()
         ],
       )),
     );
   }
 
-  Padding Textfield() {
+  Padding Have() {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15,vertical: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text("Dont have account ?",
+            style: TextStyle(
+            fontSize: 15,
+            color: Colors.grey,
+          ),),
+            SizedBox(width: 15,),
+            GestureDetector(
+              onTap: null,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Sign up",
+                style: TextStyle(
+                fontSize: 16,
+                color: const Color.fromARGB(255, 23, 99, 240),
+                fontWeight: FontWeight.bold
+                            ),),
+              ),
+            ),
+          ],
+        ),
+      );
+  }
+
+  Padding Login_Button() {
+    return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: InkWell(
+            onTap: null,
+            child: Container(
+              alignment: Alignment.center,
+              width: 265,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Color(0xFF225AEB),
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Text("Log in",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+              ),),
+            ),
+          ),
+        );
+  }
+
+  Padding Textfield(TextEditingController controller, AssetImage icon, String type,FocusNode focusNode) {
     return Padding(
           padding: EdgeInsets.symmetric(horizontal: 25,vertical: 15),
           child: Container(
@@ -43,14 +105,14 @@ class _LoginScreenState extends State<LoginScreen> {
                 color: Colors.white, borderRadius: BorderRadius.circular(20)),
             child: TextField(
               style: TextStyle(fontSize: 18, color: Colors.black),
-              controller: null,
-              focusNode: null,
+              controller: controller,
+              focusNode: focusNode,
               decoration: InputDecoration(
-                  hintText: "username",
+                  hintText: type,
                   hintStyle: TextStyle(color:  Color.fromARGB(255, 145, 144, 144)),
                   prefixIcon: Image(
-                    width: 80,
-                    image: AssetImage('assets/images/mail_icon.png')),
+                    width: 70,
+                    image: icon),
                   contentPadding:
                       EdgeInsets.symmetric(horizontal: 100, vertical: 15),
                   enabledBorder: OutlineInputBorder(
