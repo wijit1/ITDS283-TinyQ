2b1bb5128e9bc0ef6b575f0135c49d7f6936fa26
Sorrakit_Bank <htoo24653@gmail.com>
2025-04-04 20:58:14 +0700

    Create add Post  screen
    Create a textfield in addPost Screen, That user can input topic , category,description

--- tinyq/lib/screen/add_post.dart
+++ tinyq/lib/screen/add_post.dart
@@ -12,8 +12,123 @@ class _AddPost_ScreenState extends State<AddPost_Screen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text("Post Page"),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(right: 300, top: 50),
+            child: Text(
+              "Post",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Add_Title(),
+          Padding(
+            padding: EdgeInsets.all(25.0),
+            child: Container(
+              height: 238,
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                    color: Color(0xFFD9D9D9), width: 2),
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Description",
+                      hintStyle: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 13),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          onPressed: null, 
+                          icon: Icon(Icons.image)),
+                        SizedBox(width: 15,),
+                        IconButton(
+                          onPressed: null, 
+                          icon: Icon(Icons.copy)),
+                        SizedBox(width: 15,),
+                        IconButton(
+                          onPressed: null, 
+                          icon: Icon(Icons.emoji_emotions)),
+                        SizedBox(width: 15,),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Padding Add_Title() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          border:
+              Border.all(color: Color(0xFFD9D9D9), width: 2),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 30,
+              backgroundImage: AssetImage('assets/images/Mr.A.jpg'),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Your Topic ?",
+                        hintStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 77, 118, 221),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 13,),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "# category",
+                        hintStyle: TextStyle(
+                            color: Colors.grey, fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
